bce60ac4dab4af308a14e71ccef3a9ab40fcad33
JHawkZZ <jeredmcferron@gmail.com>
2023-06-25 21:22:16 -0700

    added clarifying comments

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,9 @@ int main( int argc, char **argv )
     flushPrintf( "ASP Bootstrapper Commencing\r\n");
     flushPrintf( "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\r\n");
 
+    // turn this process into sh and then FBA - QUI will wait on sh, because it inherits this
+    // boot strapper's process id. SH will wait on FBA. The 32 megs will be freed when this process space is wiped
+    // to launch sh.
     char commandWithArgs[ 256 ] = { 0 };
     snprintf( commandWithArgs, 255, "%s %s", "/mnt/hdisk/ASPh/hack/bin/fba/fba029743_asp", argv[ 1 ] );
     int result = execl( "/system/bin/sh", "sh", "-c", commandWithArgs, NULL );
